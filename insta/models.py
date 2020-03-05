db8bcef3f5783150f7a627ed6de8cb36aaf624ec
SheilaBirgen <jeronobergen@gmail.com>
2020-03-05 07:39:19 +0300

    add class profile and class image to models.py

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class Image(db.Model):
+class Image(models.Model):
+    image = CloudinaryField('images')
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    caption = models.TextField()
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    posted_at = models.DateTimeField(auto_now=True)
+
+
+    def save_image(self):
+        return self.save()
+  
+    def delete_image(self):
+        return self.delete()
+
+    def update_image(self):
+        return self.update()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+c
