2ac41035d15f3ffdccbd953e42d92f2f538e0955
SheilaBirgen <jeronobergen@gmail.com>
2020-03-08 15:42:23 +0300

    Add test for Profile class

--- insta/tests.py
+++ insta/tests.py
@@ -2,5 +2,15 @@ from django.test import TestCase
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
-    def setup()
+    def setup(self):
+        self.user = User(username='user', email='email')
+        self.user.save()
+        self.profile = Profile(image='image', bio='bio', user=self.user)
         
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
